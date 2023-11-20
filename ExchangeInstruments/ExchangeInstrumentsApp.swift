65532372ca9e13f05b7995f3d948bbb40f17e44f
Dima_68 <drdima68@yandex.ua>
2023-11-20 15:51:33 +0300

    Create project

---
+++ ExchangeInstruments/ExchangeInstrumentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeInstrumentsApp.swift
+//  ExchangeInstruments
+//
+//  Created by Дмитрий Татаринцев on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeInstrumentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
